6a4fb3cbc9e1fcafee665ef5ba56a9d9709ed575
its-priya <priyamissu1999@gmail.com>
2021-11-01 16:33:38 +0530

    Initialize project

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+import { BrowserRouter as Router, Switch} from "react-router-dom"
+
+// import Chats from "./Chats"
+// import Login from "./Login"
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'Avenir' }}>
+      <Router>
+          <Switch>
+            {/* <Route path="/chats" component={Chats} />
+            <Route path="/" component={Login} /> */}
+          </Switch>
+      </Router>
+    </div>
+  )
+}
+
+export default App
